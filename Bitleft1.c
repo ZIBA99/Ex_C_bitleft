639c86e7cf73ce900bcc085cc8c8e8d6596c0adc
ZIBA99 <97379804+ZIBA99@users.noreply.github.com>
2025-04-24 13:47:26 +0900

    Add files via upload

---
+++ Bitleft1.c
@@ -0,0 +1,15 @@
+//#include <stdio.h>
+
+/*int main()
+{
+	int num = 15; // 00000000 00000000 00000000 000011111
+
+	int result1 = num << 1;
+	int result2 = num << 2;
+	int result3 = num << 3;
+
+	printf("1Ä­ ÀÌµ¿ °á°ú: %d \n", result1);//numÀÇ ºñÆ® ¿­À» ¿ŞÂÊÀ¸·Î 1Ä­½Ä ÀÌµ¿
+	printf("2Ä­ ÀÌµ¿ °á°ú: %d \n", result2);//numÀÇ ºñÆ® ¿­À» ¿ŞÂÊÀ¸·Î 2Ä­½Ä ÀÌµ¿
+	printf("3Ä­ ÀÌµ¿ °á°ú: %d \n", result3);//numÀÇ ºñÆ® ¿­À» ¿ŞÂÊÀ¸·Î 3Ä­½Ä ÀÌµ¿
+	return 0;
+}*/
